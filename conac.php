673d07a00aae566ae18d59b7333d75b119cfc7a3
CesarAugusto77 <cesara.monroyg.21@gmail.com>
2017-08-16 11:13:17 -0500

    DIF
    PAGINA DIF

---
+++ conac.php
@@ -0,0 +1,326 @@
+<?php 
+include('assets/inc/top.inc');
+include('assets/inc/nav.inc');
+ ?>
+
+				<header id="header">
+					<div class="logo container">
+						<div>
+							<h1><a href="#" id="logo">Ley general de Contabilidad  Gubernamental</a></h1>
+						</div>
+					</div>	
+				</header>
+
+				<article class="bloqueArticulo">
+					<h2>CUMPLIMIENTO DEL TITULO V DE LA LEY GENERAL DE CONTABILIDAD GUBERNAMENTAL. PRIMER TRIMESTRE 2015</h2>
+					<br>
+					<p class="pArticulo">
+					I.- De la Información Financiera Relativa a la Elaboración de las Iniciativas de Ley de Ingresos y los Proyectos de Presupuesto de Egreso.
+					</p>
+					<div class="cuadroArticulo">
+					<li><a href="pdf/1erTrimestre_2015/11_Calendario_de_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 11. Calendario de Presupuesto de Egresos.
+					</a></li>
+					
+					<li><a href="pdf/1erTrimestre_2015/12_Calendario_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 12. Calendario de Ingresos.</i>
+					</a></li>
+
+					<li><a href="pdf/1erTrimestre_2015/13_Ley_de_Ingresos_y_del_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 13. Ley de Ingresos y del Presupuesto de Egresos.
+					</a></li>
+
+					<li><a href="pdf/1erTrimestre_2015/14_Proyecto_del_Presupuesto_de_Egresos_Armonizado.pdf">
+						<i class="fa fa-file-pdf-o"></i> 14. Proyecto del Presupuesto de Egresos Armonizado.
+					</a></li>
+
+					<li><a href="pdf/1erTrimestre_2015/15_Iniciativa_de_Ley_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 15. Iniciativa de Ley de Ingresos.
+					</a></li>
+
+
+					</div>
+
+					<br>
+
+					</div>
+
+
+					
+				</article>
+
+								<article class="bloqueArticulo">
+					<h2>CUMPLIMIENTO DEL TITULO V DE LA LEY GENERAL DE CONTABILIDAD GUBERNAMENTAL. SEGUNDO TRIMESTRE 2015</h2>
+					<br>
+					<p class="pArticulo">
+					I.- De la Información Financiera Relativa a la Elaboración de las Iniciativas de Ley de Ingresos y los Proyectos de Presupuesto de Egreso.
+					</p>
+					<div class="cuadroArticulo">
+					<li><a href="pdf/2doTrimestre_2015/11_Calendario_de_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 11. Calendario de Presupuesto de Egresos.
+					</a></li>
+					
+					<li><a href="pdf/2doTrimestre_2015/12_Calendario_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 12. Calendario de Ingresos.</i>
+					</a></li>
+
+					<li><a href="pdf/2doTrimestre_2015/13_Ley_de_Ingresos_y_del_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 13. Ley de Ingresos y del Presupuesto de Egresos.
+					</a></li>
+
+					<li><a href="pdf/2doTrimestre_2015/14_Proyecto_del_Presupuesto_de_Egresos_Armonizado.pdf">
+						<i class="fa fa-file-pdf-o"></i> 14. Proyecto del Presupuesto de Egresos Armonizado.
+					</a></li>
+
+					<li><a href="pdf/2doTrimestre_2015/15_Iniciativa_de_Ley_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 15. Iniciativa de Ley de Ingresos.
+					</a></li>
+
+
+					</div>
+
+					<br>
+
+					</div>
+
+
+					
+				</article>
+
+				</article>
+
+								<article class="bloqueArticulo">
+					<h2>CUMPLIMIENTO DEL TITULO V DE LA LEY GENERAL DE CONTABILIDAD GUBERNAMENTAL. TERCER TRIMESTRE 2015</h2>
+					<br>
+					<p class="pArticulo">
+					I.- De la Información Financiera Relativa a la Elaboración de las Iniciativas de Ley de Ingresos y los Proyectos de Presupuesto de Egreso.
+					</p>
+					<div class="cuadroArticulo">
+					<li><a href="pdf/3erTrimestre_2015/11_Calendario_de_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 11. Calendario de Presupuesto de Egresos.
+					</a></li>
+					
+					<li><a href="pdf/3erTrimestre_2015/12_Calendario_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 12. Calendario de Ingresos.</i>
+					</a></li>
+
+					<li><a href="pdf/3erTrimestre_2015/13_Ley_de_Ingresos_y_del_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 13. Ley de Ingresos y del Presupuesto de Egresos.
+					</a></li>
+
+					<li><a href="pdf/3erTrimestre_2015/14_Proyecto_del_Presupuesto_de_Egresos_Armonizado.pdf">
+						<i class="fa fa-file-pdf-o"></i> 14. Proyecto del Presupuesto de Egresos Armonizado.
+					</a></li>
+
+					<li><a href="pdf/3erTrimestre_2015/15_Iniciativa_de_Ley_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 15. Iniciativa de Ley de Ingresos.
+					</a></li>
+
+
+					</div>
+
+					<br>
+
+					</div>
+
+
+					
+				</article>
+
+				</article>
+
+								<article class="bloqueArticulo">
+					<h2>CUMPLIMIENTO DEL TITULO V DE LA LEY GENERAL DE CONTABILIDAD GUBERNAMENTAL. CUARTO TRIMESTRE 2015</h2>
+					<br>
+					<p class="pArticulo">
+					I.- De la Información Financiera Relativa a la Elaboración de las Iniciativas de Ley de Ingresos y los Proyectos de Presupuesto de Egreso.
+					</p>
+					<div class="cuadroArticulo">
+					<li><a href="pdf/4toTrimestre_2015/11_Calendario_de_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 11. Calendario de Presupuesto de Egresos.
+					</a></li>
+					
+					<li><a href="pdf/4toTrimestre_2015/12_Calendario_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 12. Calendario de Ingresos.</i>
+					</a></li>
+
+					<li><a href="pdf/4toTrimestre_2015/13_Ley_de_Ingresos_y_del_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 13. Ley de Ingresos y del Presupuesto de Egresos.
+					</a></li>
+
+					<li><a href="pdf/4toTrimestre_2015/14_Proyecto_del_Presupuesto_de_Egresos_Armonizado.pdf">
+						<i class="fa fa-file-pdf-o"></i> 14. Proyecto del Presupuesto de Egresos Armonizado.
+					</a></li>
+
+					<li><a href="pdf/4toTrimestre_2015/15_Iniciativa_de_Ley_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 15. Iniciativa de Ley de Ingresos.
+					</a></li>
+
+
+					</div>
+
+					<br>
+
+					</div>
+
+
+					
+				</article>
+
+				<article class="bloqueArticulo">
+					<h2>CUMPLIMIENTO DEL TITULO V DE LA LEY GENERAL DE CONTABILIDAD GUBERNAMENTAL. PRIMER TRIMESTRE 2016</h2>
+					<br>
+					<p class="pArticulo">
+					I.- De la Información Financiera Relativa a la Elaboración de las Iniciativas de Ley de Ingresos y los Proyectos de Presupuesto de Egreso.
+					</p>
+					<div class="cuadroArticulo">
+					<li><a href="pdf/1erTrimestre/11_Calendario_de_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 11. Calendario de Presupuesto de Egresos.
+					</a></li>
+					
+					<li><a href="pdf/1erTrimestre/12_Calendario_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 12. Calendario de Ingreso.</i>
+					</a></li>
+
+					<li><a href="pdf/1erTrimestre/13_Ley_de_Ingresos_y_del_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 13. Ley de Ingresos y del Presupuesto de Egresos.
+					</a></li>
+
+					<li><a href="pdf/1erTrimestre/14_Proyecto_del_Presupuesto_de_Egresos_Armonizado.pdf">
+						<i class="fa fa-file-pdf-o"></i> 14. Proyecto del Presupuesto de Egresos Armonizado.
+					</a></li>
+
+					<li><a href="pdf/1erTrimestre/15_Iniciativa_de_Ley_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 15. Iniciativa de Ley de Ingresos.
+					</a></li>
+
+
+					</div>
+
+					<br>
+
+					</div>
+
+
+					
+				</article>
+				<article class="bloqueArticulo">
+					<h2>CUMPLIMIENTO DEL TITULO V DE LA LEY GENERAL DE CONTABILIDAD GUBERNAMENTAL. SEGUNDO TRIMESTRE 2016</h2>
+					<br>
+					<p class="pArticulo">
+					I.- De la Información Financiera Relativa a la Elaboración de las Iniciativas de Ley de Ingresos y los Proyectos de Presupuesto de Egreso.
+					</p>
+					<div class="cuadroArticulo">
+					<li><a href="pdf/2doTrimestre/11_Calendario_de_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 11. Calendario de Presupuesto de Egresos.
+					</a></li>
+					
+					<li><a href="pdf/2doTrimestre/12_Calendario_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 12. Calendario de Ingreso.</i>
+					</a></li>
+
+					<li><a href="pdf/2doTrimestre/13_Ley_de_Ingresos_y_del_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 13. Ley de Ingresos y del Presupuesto de Egresos.
+					</a></li>
+
+					<li><a href="pdf/2doTrimestre/14_Proyecto_del_Presupuesto_de_Egresos_Armonizado.pdf">
+						<i class="fa fa-file-pdf-o"></i> 14. Proyecto del Presupuesto de Egresos Armonizado.
+					</a></li>
+
+					<li><a href="pdf/2doTrimestre/15_Iniciativa_de_Ley_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 15. Iniciativa de Ley de Ingresos.
+					</a></li>
+
+
+					</div>
+
+					<br>
+
+					</div>
+					
+
+					
+				</article>
+
+				<!-- Conac 3er Trimestre 2016(Luis)31/10/2016 -->
+
+				<article class="bloqueArticulo">
+					<h2>CUMPLIMIENTO DEL TITULO V DE LA LEY GENERAL DE CONTABILIDAD GUBERNAMENTAL. TERCER TRIMESTRE 2016</h2>
+					<br>
+					<p class="pArticulo">
+					I.- De la Información Financiera Relativa a la Elaboración de las Iniciativas de Ley de Ingresos y los Proyectos de Presupuesto de Egreso.
+					</p>
+					<div class="cuadroArticulo">
+					<li><a href="pdf/3erTrimestre/11_Calendario_de_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 11. Calendario de Presupuesto de Egresos.
+					</a></li>
+					
+					<li><a href="pdf/3erTrimestre/12_Calendario_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 12. Calendario de Ingreso.</i>
+					</a></li>
+
+					<li><a href="pdf/3erTrimestre/13_Ley_de_Ingresos_y_del_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 13. Ley de Ingresos y del Presupuesto de Egresos.
+					</a></li>
+
+					<li><a href="pdf/3erTrimestre/14_Proyecto_del_Presupuesto_de_Egresos_Armonizado.pdf">
+						<i class="fa fa-file-pdf-o"></i> 14. Proyecto del Presupuesto de Egresos Armonizado.
+					</a></li>
+
+					<li><a href="pdf/3erTrimestre/15_Iniciativa_de_Ley_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 15. Iniciativa de Ley de Ingresos.
+					</a></li>
+
+
+					</div>
+
+					<br>
+
+					</div>
+					
+
+					
+				</article>
+
+				<!-- Conac 4to Trimestre 2016(Luis)31/10/2016 -->
+
+				<article class="bloqueArticulo">
+					<h2>CUMPLIMIENTO DEL TITULO V DE LA LEY GENERAL DE CONTABILIDAD GUBERNAMENTAL. CUARTO TRIMESTRE 2016</h2>
+					<br>
+					<p class="pArticulo">
+					I.- De la Información Financiera Relativa a la Elaboración de las Iniciativas de Ley de Ingresos y los Proyectos de Presupuesto de Egreso.
+					</p>
+					<div class="cuadroArticulo">
+					<li><a href="pdf/4toTrimestre_2016/11_Calendario_de_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 11. Calendario de Presupuesto de Egresos.
+					</a></li>
+					
+					<li><a href="pdf/4toTrimestre_2016/12_Calendario_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 12. Calendario de Ingreso.</i>
+					</a></li>
+
+					<li><a href="pdf/4toTrimestre_2016/13_Ley_de_Ingresos_y_del_Presupuesto_de_Egresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 13. Ley de Ingresos y del Presupuesto de Egresos.
+					</a></li>
+
+					<li><a href="pdf/4toTrimestre_2016/14_Proyecto_del_Presupuesto_de_Egresos_Armonizado.pdf">
+						<i class="fa fa-file-pdf-o"></i> 14. Proyecto del Presupuesto de Egresos Armonizado.
+					</a></li>
+
+					<li><a href="pdf/4toTrimestre_2016/15_Iniciativa_de_Ley_de_Ingresos.pdf">
+						<i class="fa fa-file-pdf-o"></i> 15. Iniciativa de Ley de Ingresos.
+					</a></li>
+
+
+					</div>
+
+					<br>
+
+					</div>
+					
+
+					
+				</article>
+ <?php 
+include("assets/inc/footer.inc");
+  ?>
---
+++ tarifas.php
@@ -0,0 +1,107 @@
+<?php 
+include('assets/inc/top.inc');
+include('assets/inc/nav.inc');
+ ?>
+
+				<header id="header">
+					<div class="logo container">
+						<div>
+							<h1><a href="#" id="logo">SERVICIOS - COSTOS</a></h1>
+						</div>
+					</div>	
+				</header>
+				<article class="bloqueArticulo">
+					<div class="cuadroNR">
+						<div class="cuadroNRc1"><img src="images/nada.png"></div>
+						<div class="cuadroNRc2"><h2>TARIFAS SAPASNIR - 2017</h2></div>
+						<div class="cuadroNRc3"><img src="images/nada.png"></div>
+					</div>
+					<br>
+
+
+						<div class="tablaTresC">
+						<table>
+							<tr>
+								<td class="cTab enca i">TARIFARIO DEL EJERCICIO FISCAL 2017</td>
+								<td class="cTab enca f">DERECHOS</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">DERECHO DE TOMA DOMESTICA  </td>
+								<td class="cTab">$6,476</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">DERECHO DE TOMA USO NO DOMESTICA</td>
+								<td class="cTab">$21,743</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">DERECHO DE CONEXIÓN DE DRENAJE DOMESTICO</td>
+								<td class="cTab">$2,170</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">DERECHO DE ALTA DE DERIVACIÓN </td>
+								<td class="cTab">$639</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">CUADRO PARA MEDIDOR</td>
+								<td class="cTab">$736</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">DERECHO DE INSTALACIÓN DE MEDIDOR </td>
+								<td class="cTab">$1,051</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">CAMBIO DE PROPIETARIO</td>
+								<td class="cTab">$346</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">CERTIFICACIÓN DE NO ADEUDO Y/O SERVICIO </td>
+								<td class="cTab">$175</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">COPIAS CERTIFICADAS </td>
+								<td class="cTab">$74</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">CORRECCIÓN DE DATOS </td>
+								<td class="cTab">$88</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">RECONEXIÓN CON MEDIDOR </td>
+								<td class="cTab">$482</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">RECONEXIÓN SIN MEDIDOR </td>
+								<td class="cTab">$824</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">GASTOS DE EJECUCIÓN</td>
+								<td class="cTab">$377</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">REGULARIZACIÓN DE TOMA USO NO DOMESTICA  </td>
+								<td class="cTab">$15,267</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">CUOTA MÍNIMA DOMESTICA  </td>
+								<td class="cTab">$124</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">CUOTA FIJA DOMESTICA </td>
+								<td class="cTab">$390</td>
+							</tr>
+							<tr>
+								<td class="cTab reca">CUOTA MÍNIMA USO NO DOMESTICO</td>
+								<td class="cTab">$322</td>
+							</tr>
+							<tr>
+								<td class="cTab reca f">CUOTA FIJA USO NO DOMESTICO </td>
+								<td class="cTab">$2,433</td>
+							</tr>
+							<tr>
+						</table>
+						</div>
+											
+				</article>
+ <?php 
+include("assets/inc/footer.inc");
+  ?>
